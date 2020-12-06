64dd6307d267cc57bddb3904732cb8a2f939b18a
Himanshu-Dalal <75560256+Himanshu-Dalal@users.noreply.github.com>
2020-12-06 13:02:27 +0530

    Add files via upload

---
+++ ninjajs.js
@@ -0,0 +1,7 @@
+let circle = document.querySelector('#circle');
+let img1 = document.querySelector('#c1');
+let img2 = document.querySelector('#c2');
+let img3 = document.querySelector('#c3');
+
+img1.addEventListener('click',function(event){
+    circle.style.background
